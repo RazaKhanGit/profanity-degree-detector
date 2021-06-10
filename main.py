6b36fd7bfde2f45f2066c86b6c622a1c41839bdc
Raza Khan <mdrazakhan2019@gmail.com>
2021-06-10 17:04:14 +0530

    Add comment to main.py

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import re
-from tkinter.constants import SEPARATOR
+import re #using regex to read the files
 
-f = open('words.txt', 'r')
-profaneWords = [tweet.rstrip('\n') for tweet in f] #reading from the tweetlist, and removing the trailing '\n'
+f = open('wordsList.txt', 'r')
+profaneWords = [tweet.rstrip('\n') for tweet in f] #reading words from the words.txt, and removing the trailing '\n'
 
 def tokenize(text): #tokenizing the input text
     list = [tweet.lower() for tweet in re.split(r'[`\-=~!@#$%^&*()_+\[\]{} ;\'\\:"|<,./<>?]', text)]
     return list
 
-fTweets = open('tweet.txt', 'r')
+fTweets = open('tweetList.txt', 'r') #reading tweets 
 tweets = [tweet.rstrip('\n') for tweet in fTweets]
 tweetTokenized = [tokenize(t) for t in tweets]
 with open('profaneDegree.txt', 'w') as f:
     for i in range(len(tweets)):
-        n = 0
+        n = 0 #number of profane words in a tweet
         for w in tweetTokenized[i]:
             if w in profaneWords:
                 n = n + 1
             else:
                 for pw in profaneWords:
-                    if w.find(pw) != -1:
+                    if w.find(pw) != -1: #if profaneword is substring of word then increase count by 1
                         n = n+1
 
         
